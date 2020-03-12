003ba7096cd4d988d9285881ad80ddfb63599544
Maxim Kostyukov <m.kostyukov@anchorfree.com>
2020-03-12 20:46:11 +0300

    Replace hardcoded db name with env

--- wait_for_db.sh
+++ wait_for_db.sh
@@ -8,6 +8,6 @@ until mysql -h mysql -u root -e '\q'; do
 done
 
 >&2 echo "Mysql is up - executing command"
-mysql -hmysql -uroot -e "CREATE DATABASE laravel"
+mysql -hmysql -uroot -e "CREATE DATABASE ${DB_DATABASE}"
 php artisan migrate --force && php artisan db:seed --class=DatabaseSeeder --force && npm install && npm run dev
 exec php artisan serve --host=0.0.0.0 --port=80
