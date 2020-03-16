c256c1718ee1448d5ed1fd87207a80302c7668c0
Maxim Kostyukov <m.kostyukov@anchorfree.com>
2020-03-16 18:33:22 +0300

    Remove all but compose

--- resources/js/components/Button.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-const Button = ({ buttonText }) => {
-    return (
-        <button type="button" className="btn btn-primary">{ buttonText }</button>
-    )
-}
-
-export default Button;
